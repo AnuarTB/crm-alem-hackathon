e606f3a18c4971154ad01ad2b247e6e0b2e69357
Anuar Talipov <anuartb@gmail.com>
2020-03-16 02:21:03 +0500

    Updates

---
+++ precache-manifest.ab5b6c429f74c82e5fe27a6534cf9fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ba6dc92d84f9949c833e211dd7cedc",
+    "url": "/crm-alem-hackathon/index.html"
+  },
+  {
+    "revision": "0d6bc5ce4dd9f349d3fa",
+    "url": "/crm-alem-hackathon/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6ee613727f04ef80aaeb",
+    "url": "/crm-alem-hackathon/static/css/main.65b68748.chunk.css"
+  },
+  {
+    "revision": "0d6bc5ce4dd9f349d3fa",
+    "url": "/crm-alem-hackathon/static/js/2.a12971fb.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/crm-alem-hackathon/static/js/2.a12971fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee613727f04ef80aaeb",
+    "url": "/crm-alem-hackathon/static/js/main.0d367339.chunk.js"
+  },
+  {
+    "revision": "2df52b05a31abd697247",
+    "url": "/crm-alem-hackathon/static/js/runtime-main.7581e402.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b82acbfa183645f760ec61e001a379f.js"
+  "/crm-alem-hackathon/precache-manifest.ab5b6c429f74c82e5fe27a6534cf9fb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crm-alem-hackathon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
